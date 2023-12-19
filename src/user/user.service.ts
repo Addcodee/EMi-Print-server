49e73e37992c3440f795138e44d2bcdf065765b5
Alibek <musicchannel0p@gmail.com>
2023-12-19 23:00:32 +0600

    made cashing findOne

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,20 @@
 import { JwtPayload } from '@auth/interfaces';
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { convertToSecondsUtil } from '@common/utils';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Role, User } from '@prisma/client';
 import { PrismaService } from '@prisma/prisma.service';
 import { genSaltSync, hashSync } from 'bcrypt';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor(
+        private readonly prismaService: PrismaService,
+        @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
+        private readonly configService: ConfigService,
+    ) {}
 
     save(user: Partial<User>) {
         const hashedPassword = this.hashPassword(user.password);
@@ -24,20 +32,38 @@ export class UserService {
         });
     }
 
-    findOne(idOrNumber: string) {
-        return this.prismaService.user.findFirst({
-            where: { OR: [{ id: idOrNumber }, { phoneNumber: idOrNumber }] },
-        });
+    async findOne(idOrNumber: string, isReset = false) {
+        if (isReset) {
+            this.cacheManager.del(idOrNumber);
+        }
+        const user = await this.cacheManager.get<User>(idOrNumber);
+        if (!user) {
+            const user = await this.prismaService.user.findFirst({
+                where: { OR: [{ id: idOrNumber }, { phoneNumber: idOrNumber }] },
+            });
+
+            if (!user) return null;
+
+            const timeOutInSec = convertToSecondsUtil(this.configService.get('JWT_EXP'));
+
+            await this.cacheManager.set(idOrNumber, user, timeOutInSec);
+
+            return user;
+        }
+        return user;
     }
 
     findAll() {
         return this.prismaService.user.findMany();
     }
 
-    delete(userId: string, user: JwtPayload) {
+    async delete(userId: string, user: JwtPayload) {
         if (user.id !== userId && !user.roles.includes(Role.ADMIN)) {
             throw new ForbiddenException();
         }
+        await Promise.all([this.cacheManager.del(userId), this.cacheManager.del(user.phoneNumber)]);
+
+        await this.cacheManager.del(userId);
 
         return this.prismaService.user.delete({ where: { id: userId }, select: { id: true } });
     }
