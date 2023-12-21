f85ad280bb01679514040f3e8d32d0ef3cf85e28
Alibek <musicchannel0p@gmail.com>
2023-12-21 16:33:15 +0600

    port

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ async function bootstrap() {
         .addBearerAuth()
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('/api/docs', app, document);
 
-    await app.listen(PORT, () => console.log(`SERVER STARTED ON PORT = ${PORT}`));
+    await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
